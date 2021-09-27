8caeb4a824ff0b1de695d321fa34750b29dc9ae3
yushachkov <a_yushachkov@mail.ru>
2021-09-28 02:42:21 +0300

    Передаю много Props через map

--- src/App.js
+++ src/App.js
@@ -4,14 +4,21 @@ import ClassCounter from './components/ClassCounter'
 import Counter from './components/Counter'
 import './styles/App.css'
 import PostItem from './components/PostItem';
+import PostList from './components/PostList';
 
 
 function App() {
-  const [value, setValue] = useState('Текст в Инпуте')
+  const [posts, setPost] = useState([
+    {id:1, title: 'JavaScript', body: 'Description'},
+    {id:2, title: 'JavaScript', body: 'Description'},
+    {id:3, title: 'JavaScript', body: 'Description'},
+
+
+  ])
 
   return (
     <div className="App">
-      <PostItem post={{id:1, title: 'JavaScript', body: 'Description'}}/>
+      <PostList posts={posts} title={'Список постов:'}/>
     </div>
   );
 }
