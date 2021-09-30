991fce6bd28a9163f87105594160bbe569e266dd
yushachkov <a_yushachkov@mail.ru>
2021-09-30 03:24:48 +0300

    Сортировка и Выпадающий списокgit add .git add .

--- src/App.js
+++ src/App.js
@@ -8,18 +8,20 @@ import PostList from './components/PostList';
 import MyButton from './components/UI/button/MyButton';
 import MyInput from './components/UI/input/MyInput'
 import PostForm from './components/PostForm'
+import MySelect from './components/select/MySelect.jsx'
 
 function App() {
   const [posts, setPosts] = useState([
-    { id: 1, title: 'JavaScript', body: 'Description' },
-    { id: 2, title: 'JavaScript', body: 'Description' },
-    { id: 3, title: 'JavaScript', body: 'Description' },
+    { id: 1, title: 'аа', body: 'бб' },
+    { id: 2, title: 'гг', body: 'аа' },
+    { id: 3, title: 'вв', body: 'яя' },
 
   ])
   const [title, setTitle] = useState('')
   const [body, setBody] = useState('')
 
-
+  const [selectedSort,setSelectedSort] = useState('')
+  
   function createPost(newPost) {
     setPosts([...posts, newPost])
   }
@@ -29,11 +31,38 @@ function App() {
     setPosts(posts.filter(p => p.id !== post.id))
 
   }
-
+  const sortPosts = (sort) => {
+    setSelectedSort(sort)
+    setPosts([...posts].sort((a,b)=> a[sort].localeCompare(b[sort])))
+    // localeCompare -  функция для сравнения строк
+  }
   return (
     <div className="App">
       <PostForm create={createPost} />
-      <PostList remove={removePost} posts={posts} title='Список постов:' />
+      <hr style={{margin: "15px 0"}}></hr>
+      <div>
+        <MySelect
+        value={selectedSort}
+        onChange={sortPosts}
+        defaultValue = {"Сортировка"}
+        options = {[
+          {
+          value: "title",
+          name: "По Названию"
+        },
+        {
+          value: "body",
+          name: "По Описанию"
+        }
+        ]}
+        />
+      </div>
+      {posts.length !== 0 
+      ?  <PostList remove={removePost} posts={posts} title='Список постов:' />
+      : <h1 style={{textAlign: 'center'}}>
+      Посты не найдены!
+      </h1>
+      }
     </div>
   );
 }
